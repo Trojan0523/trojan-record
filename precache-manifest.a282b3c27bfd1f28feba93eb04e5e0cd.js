f8e4dffb2ae56b059e3094acad52862b49895962
Trojan0523 <1153532327@qq.com>
2020-10-13 17:36:25 +0800

    deploy

---
+++ precache-manifest.a282b3c27bfd1f28feba93eb04e5e0cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c31ba2f9c27cd8a331",
+    "url": "/trojan-record/css/app.fee32a1c.css"
+  },
+  {
+    "revision": "d252a8137748d7979c250ff48b67ae6e",
+    "url": "/trojan-record/index.html"
+  },
+  {
+    "revision": "b0c31ba2f9c27cd8a331",
+    "url": "/trojan-record/js/app.63d22d5e.js"
+  },
+  {
+    "revision": "648bccb45e2bd74fafc8",
+    "url": "/trojan-record/js/chunk-vendors.b89d507d.js"
+  },
+  {
+    "revision": "d9ebf5af451738821cffb88858f9a04d",
+    "url": "/trojan-record/manifest.json"
+  },
+  {
+    "revision": "d8ac71be9246a22b5064151bb6ac1098",
+    "url": "/trojan-record/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/trojan-record/robots.txt"
+  }
+]);
